59e6caf1843d4d356ac09e770fae87667d4d6a76
marcduranxanco <marcduranxanco@hotmail.com>
2021-09-23 00:19:52 +0200

    GameTest :: update game test adding repository

--- tests/Games/GameTest.php
+++ tests/Games/GameTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use App\Games\Game;
+use Test\Games\GameTestRepository;
 use App\Users\User;
 
 class GameTest extends TestCase
@@ -11,14 +12,15 @@ class GameTest extends TestCase
     private User $user2;
 
     protected function setUp(): void {
-        $this->user1 = new User("11111", "User1");
-        $this->user2 = new User("22222", "User2");
+        $this->user1 = new User("1", "User1");
+        $this->user2 = new User("2", "User2");
         $users = [$this->user1, $this->user2];
-        $this->game = New Game($users,3,2);
+        $gameRepository = new GameTestRepository("game0", $users, "", false);
+        $this->game = New Game($users,3,2,$gameRepository);
     }
 
     public function testSetWinner(){
         $this->setUp();
-        self::assertEquals($this->game->setWinner($this->user1), "User1");
+        self::assertEquals($this->game->setWinner($this->user1), "1");
     }
 }
