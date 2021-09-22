9dc8feda48a05d316fe840293a0e38072dc9af56
marcduranxanco <marcduranxanco@hotmail.com>
2021-09-23 00:18:19 +0200

    Game :: update and add savegame method

--- src/Games/Game/Domain/Game.php
+++ src/Games/Game/Domain/Game.php
@@ -4,25 +4,30 @@ namespace App\Games;
 
 use App\Users\User;
 use Exception;
+use GameRepository;
 use iGame;
 
 class Game implements iGame
 {
     private Array $users;
     private string $idGame;
-    private User $winner;
+    private string $idWinner;
     private int $maxUsers;
     private int $minUsers;
+    private GameRepository $gameRepository;
 
     public function __construct(
         Array $p_users,
         int $p_maxUsers,
-        int $p_minUsers
+        int $p_minUsers,
+        GameRepository $p_gameRepository
     ) {
-        $this->idGame   = uniqid();
-        $this->maxUsers = $p_maxUsers;
-        $this->minUsers = $p_minUsers;
-        $this->users    = $p_users;
+        $this->idGame           = uniqid();
+        $this->maxUsers         = $p_maxUsers;
+        $this->minUsers         = $p_minUsers;
+        $this->users            = $p_users;
+        $this->gameRepository   = $p_gameRepository;
+
         if(!$this->userValidation())
             throw new Exception("Error Validating the users", 1);
     }
@@ -43,15 +48,23 @@ class Game implements iGame
 
     public function setWinner(User $p_user): string
     {
-        $this->winner = $p_user;
-        return $this->winner->getName();
+        $this->idWinner = $p_user->getId();
+        return $this->idWinner;
     }
 
-    private function saveGame(): void
+    public function saveGame(): bool
     {
-        $this->idGame;
-        $this->users;
-        $this->winner;
+        try {
+            $this->gameRepository->saveGame(
+                $this->idGame,
+                $this->users,
+                $this->idWinner,
+                $this->isFinished()
+            );
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
     }
 
     private function isFinished(): bool
