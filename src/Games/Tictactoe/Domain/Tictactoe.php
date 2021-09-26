271e11a3df095693b6dfbe186f252718eeaf5b42
marcduranxanco <marcduranxanco@hotmail.com>
2021-09-26 22:40:47 +0200

    TicTacToe :: adding update repository

--- src/Games/Tictactoe/Domain/Tictactoe.php
+++ src/Games/Tictactoe/Domain/Tictactoe.php
@@ -5,10 +5,16 @@ namespace App\Games;
 use GameRepository;
 class Tictactoe extends Game
 {
+    public TictactoeBoard $board;
+
     public function __construct(
         Array $p_users,
-        GameRepository $p_gameRepository
+        GameRepository $p_gameRepository,
+        TictactoeBoard $p_board
     ) {
+        $this->board = $p_board;
         parent::__construct($p_users, 2, 2, $p_gameRepository);
     }
+
+    
 }
